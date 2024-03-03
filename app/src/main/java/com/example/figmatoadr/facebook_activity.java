7208a7a533d849902a4ed4bb3765c63006151f81
Way-curious <tuyentieple@gmail.com>
2024-03-03 16:07:54 +0700

    Convert Figma Design to Android

---
+++ app/src/main/java/com/example/figmatoadr/facebook_activity.java
@@ -0,0 +1,102 @@
+package com.example.figmatoadr;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		facebook
+	 *	@date 		Sunday 03rd of March 2024 08:38:56 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class facebook_activity extends Activity {
+
+	
+	private View _bg__facebook;
+	private TextView facebook;
+	private View rectangle_1;
+	private TextView mobile_number_or_email_address;
+	private View rectangle_2;
+	private TextView password;
+	private ImageView rectangle_3;
+	private TextView log_in;
+	private TextView forgotten_password_;
+	private ImageView line_1;
+	private ImageView line_2;
+	private TextView or;
+	private View rectangle_4;
+	private TextView create_new_account;
+	private TextView about;
+	private TextView help;
+	private TextView meta;
+	private TextView _2022;
+	private TextView more;
+	private View _bg__group_ek1;
+	private View _bg__group_ek3;
+	private View _bg__group_ek5;
+	private ImageView vector;
+	private View _bg___button_ek1;
+	private View _bg__arrow_back_ek1;
+	private ImageView vector_ek1;
+	private ImageView vector_ek2;
+	private TextView button_ek2;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.facebook);
+
+		
+		_bg__facebook = (View) findViewById(R.id._bg__facebook);
+		facebook = (TextView) findViewById(R.id.facebook);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		mobile_number_or_email_address = (TextView) findViewById(R.id.mobile_number_or_email_address);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		password = (TextView) findViewById(R.id.password);
+		rectangle_3 = (ImageView) findViewById(R.id.rectangle_3);
+		log_in = (TextView) findViewById(R.id.log_in);
+		forgotten_password_ = (TextView) findViewById(R.id.forgotten_password_);
+		line_1 = (ImageView) findViewById(R.id.line_1);
+		line_2 = (ImageView) findViewById(R.id.line_2);
+		or = (TextView) findViewById(R.id.or);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		create_new_account = (TextView) findViewById(R.id.create_new_account);
+		about = (TextView) findViewById(R.id.about);
+		help = (TextView) findViewById(R.id.help);
+		meta = (TextView) findViewById(R.id.meta);
+		_2022 = (TextView) findViewById(R.id._2022);
+		more = (TextView) findViewById(R.id.more);
+		_bg__group_ek1 = (View) findViewById(R.id._bg__group_ek1);
+		_bg__group_ek3 = (View) findViewById(R.id._bg__group_ek3);
+		_bg__group_ek5 = (View) findViewById(R.id._bg__group_ek5);
+		vector = (ImageView) findViewById(R.id.vector);
+		_bg___button_ek1 = (View) findViewById(R.id._bg___button_ek1);
+		_bg__arrow_back_ek1 = (View) findViewById(R.id._bg__arrow_back_ek1);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		button_ek2 = (TextView) findViewById(R.id.button_ek2);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
